88fcce209d6789711cf0e4ff36586d67d24c46ee
robpointmarc <randerson@pointmarc.com>
2015-09-17 09:43:09 -0700

    #id 5640 update SiteCatalyst s_code to version 4.1

--- sitecatalyst_tag.js
+++ sitecatalyst_tag.js
@@ -2,7 +2,7 @@
 Copyright 1996-2013 Adobe, Inc. All Rights Reserved
 More info available at http://www.omniture.com */
 /************************ ADDITIONAL FEATURES ************************
-     Version 4.02
+     Version 4.1
 */
 
 var s_account="##UTVARconfig_reportsuite##";
@@ -24,12 +24,11 @@ var ##UTVARconfig_sobject##=s_gi(s_account);
 ##UTVARconfig_sobject##.trackingServer="##UTVARconfig_server##";
 ##UTVARconfig_sobject##.trackingServerSecure="##UTVARconfig_serversecure##";
 ##UTVARconfig_sobject##.dc=122;
-
 /************************** CONFIG SECTION **************************/
 /* You may add or alter any code config here. */
 /* Conversion Config */
 s.currencyCode="USD"
-s.s_codeVersion="4.02"
+s.s_codeVersion="4.1"
 /* Link Tracking Config */
 s.trackDownloadLinks=true
 s.trackExternalLinks=true
@@ -385,31 +384,37 @@ else {
 	  s.c_w('core_email',"",-1);
 	}
 	
-	if(s.events&&s.events.match(/(event32)|(event51)|(event52)|(event53)|(purchase)/)){
-		if(s.eVar43&&s.eVar43!=""){
-			if(s.c_r('e43C2')!=s.eVar43){
-			var e=new Date();
-			e.setTime(e.getTime()+(730*86400000));
-			s.c_w('e43C2',s.eVar43,e);
-			s.events=s.apl(s.events,'event68',',',2);
-			}
-		}
-		if(s.state&&s.state!=""){
-			if(s.c_r('eStC2')!=s.state){
-			var e=new Date();
-			e.setTime(e.getTime()+(730*86400000));
-			s.c_w('eStC2',s.state,e);
-			s.events=s.apl(s.events,'event68',',',2);
-			}
-		}
-		if(s.zip&&s.zip!=""){
-			if(s.c_r('eZpC2')!=s.zip){
-			var e=new Date();
-			e.setTime(e.getTime()+(730*86400000));
-			s.c_w('eZpC2',s.zip,e);
-			s.events=s.apl(s.events,'event68',',',2);
-			}
-		}
+
+		var e = new Date(), // date object
+        e43c2 = s.c_r( 'e43C2' ), //reads e43C2 cookie
+        encryption, // to be used as Base64 Encoding of SHA1-Hashed eVar43 or existing cookie to be set in eVar43
+        hash, // to be used as SHA1 Hash of either eVar43 or the existing cookie
+        _isCookiePlaintext = e43c2.search( /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i ) > -1; //checks to see if cookie is in plaintext email address format 
+
+	
+	if(s.events&&s.events.match(/(event32)|(event51)|(event52)|(event53)|(purchase)/)){ // if we're on a page with any of these events
+    if(s.eVar43&&s.eVar43!=""){ // if eVar43 exists and has a string value
+        hashedVal( s.eVar43 ); // hash eVar43  
+        if( e43c2 != encryption ) { // if the hash value of eVar43 is not the same as the e43c2 cookie
+            encodeCookie( true ); // encode and set the cookie and set event68
+        }
+    }
+    if(s.state&&s.state!=""){
+        if(s.c_r('eStC2')!=s.state){
+            var e=new Date();
+            e.setTime(e.getTime()+(730*86400000));
+            s.c_w('eStC2',s.state,e);
+            s.events=s.apl(s.events,'event68',',',2);
+        }
+    }
+    if(s.zip&&s.zip!=""){
+        if(s.c_r('eZpC2')!=s.zip){
+            var e=new Date();
+            e.setTime(e.getTime()+(730*86400000));
+            s.c_w('eZpC2',s.zip,e);
+            s.events=s.apl(s.events,'event68',',',2);
+        }
+    }
 		if(s.eVar34&&s.eVar34!=""){
 			if(s.c_r('e34C2')!=s.eVar34){
 			var e=new Date();
@@ -418,7 +423,9 @@ else {
 			s.events=s.apl(s.events,'event68',',',2);
 			}
 		}
-	}
+} else { // if not on a page with any of those events
+  cookieCheck(); // check to see if the cookie is in plaintext. if it is encode the cookie without setting it or event68 
+}
 	s.plugins="";
 	s.tnt = s.trackTNT();
 	
@@ -694,6 +701,63 @@ s.c_w('svi_dec',sVI,b);
 
 return sVI;
 }
+
+/**
+ * Check to see if the e43c2 cookie is in plaintext format
+ * If it is, encode and set it, then set event 68
+ * @var  {_isCookiePlainText} boolean   cookie format
+ */
+function cookieCheck(){
+    if( _isCookiePlaintext ){ 
+        hashedVal( e43c2 );
+        encodeCookie( false );
+    }
+}
+
+/**
+ * Create SHA1 Hash
+ * @param   {val}           string      value                                   either E43C2 cookie or s.eVar43 value
+ * @var     {hash}          string      SHA1 hash                               either E43C2 cookie or s.eVar43 value
+ * @var     {encryption}    string      Base64 Encoding of SHA1-Hashed value    either E43C2 cookie or s.eVar43 value
+ */
+function hashedVal( val ){
+    hash = CryptoJS.SHA1( val );
+    encryption = CryptoJS.enc.Base64.stringify( hash );
+}
+
+/**
+ * Write the cookie with new encoded cookie value and timestamp
+ * Set the cookie
+ * Set event68
+ * @param  { _isNewcookie } boolean   sets the cookie and event68
+ */
+function encodeCookie( _isNewCookie ){
+    e.setTime( e.getTime() + ( 730*86400000 ) ); 
+    s.c_w( 'e43C2', encryption, e );
+    if( _isNewCookie ){
+        s.events = s.apl( s.events,'event68',',',2 );
+    }
+}
+
+/*
+CryptoJS v3.1.2
+code.google.com/p/crypto-js
+(c) 2009-2013 by Jeff Mott. All rights reserved.
+code.google.com/p/crypto-js/wiki/License
+*/
+var CryptoJS=CryptoJS||function(e,m){var p={},j=p.lib={},l=function(){},f=j.Base={extend:function(a){l.prototype=this;var c=new l;a&&c.mixIn(a);c.hasOwnProperty("init")||(c.init=function(){c.$super.init.apply(this,arguments)});c.init.prototype=c;c.$super=this;return c},create:function(){var a=this.extend();a.init.apply(a,arguments);return a},init:function(){},mixIn:function(a){for(var c in a)a.hasOwnProperty(c)&&(this[c]=a[c]);a.hasOwnProperty("toString")&&(this.toString=a.toString)},clone:function(){return this.init.prototype.extend(this)}},
+n=j.WordArray=f.extend({init:function(a,c){a=this.words=a||[];this.sigBytes=c!=m?c:4*a.length},toString:function(a){return(a||h).stringify(this)},concat:function(a){var c=this.words,q=a.words,d=this.sigBytes;a=a.sigBytes;this.clamp();if(d%4)for(var b=0;b<a;b++)c[d+b>>>2]|=(q[b>>>2]>>>24-8*(b%4)&255)<<24-8*((d+b)%4);else if(65535<q.length)for(b=0;b<a;b+=4)c[d+b>>>2]=q[b>>>2];else c.push.apply(c,q);this.sigBytes+=a;return this},clamp:function(){var a=this.words,c=this.sigBytes;a[c>>>2]&=4294967295<<
+32-8*(c%4);a.length=e.ceil(c/4)},clone:function(){var a=f.clone.call(this);a.words=this.words.slice(0);return a},random:function(a){for(var c=[],b=0;b<a;b+=4)c.push(4294967296*e.random()|0);return new n.init(c,a)}}),b=p.enc={},h=b.Hex={stringify:function(a){var c=a.words;a=a.sigBytes;for(var b=[],d=0;d<a;d++){var f=c[d>>>2]>>>24-8*(d%4)&255;b.push((f>>>4).toString(16));b.push((f&15).toString(16))}return b.join("")},parse:function(a){for(var c=a.length,b=[],d=0;d<c;d+=2)b[d>>>3]|=parseInt(a.substr(d,
+2),16)<<24-4*(d%8);return new n.init(b,c/2)}},g=b.Latin1={stringify:function(a){var c=a.words;a=a.sigBytes;for(var b=[],d=0;d<a;d++)b.push(String.fromCharCode(c[d>>>2]>>>24-8*(d%4)&255));return b.join("")},parse:function(a){for(var c=a.length,b=[],d=0;d<c;d++)b[d>>>2]|=(a.charCodeAt(d)&255)<<24-8*(d%4);return new n.init(b,c)}},r=b.Utf8={stringify:function(a){try{return decodeURIComponent(escape(g.stringify(a)))}catch(c){throw Error("Malformed UTF-8 data");}},parse:function(a){return g.parse(unescape(encodeURIComponent(a)))}},
+k=j.BufferedBlockAlgorithm=f.extend({reset:function(){this._data=new n.init;this._nDataBytes=0},_append:function(a){"string"==typeof a&&(a=r.parse(a));this._data.concat(a);this._nDataBytes+=a.sigBytes},_process:function(a){var c=this._data,b=c.words,d=c.sigBytes,f=this.blockSize,h=d/(4*f),h=a?e.ceil(h):e.max((h|0)-this._minBufferSize,0);a=h*f;d=e.min(4*a,d);if(a){for(var g=0;g<a;g+=f)this._doProcessBlock(b,g);g=b.splice(0,a);c.sigBytes-=d}return new n.init(g,d)},clone:function(){var a=f.clone.call(this);
+a._data=this._data.clone();return a},_minBufferSize:0});j.Hasher=k.extend({cfg:f.extend(),init:function(a){this.cfg=this.cfg.extend(a);this.reset()},reset:function(){k.reset.call(this);this._doReset()},update:function(a){this._append(a);this._process();return this},finalize:function(a){a&&this._append(a);return this._doFinalize()},blockSize:16,_createHelper:function(a){return function(c,b){return(new a.init(b)).finalize(c)}},_createHmacHelper:function(a){return function(b,f){return(new s.HMAC.init(a,
+f)).finalize(b)}}});var s=p.algo={};return p}(Math);
+(function(){var e=CryptoJS,m=e.lib,p=m.WordArray,j=m.Hasher,l=[],m=e.algo.SHA1=j.extend({_doReset:function(){this._hash=new p.init([1732584193,4023233417,2562383102,271733878,3285377520])},_doProcessBlock:function(f,n){for(var b=this._hash.words,h=b[0],g=b[1],e=b[2],k=b[3],j=b[4],a=0;80>a;a++){if(16>a)l[a]=f[n+a]|0;else{var c=l[a-3]^l[a-8]^l[a-14]^l[a-16];l[a]=c<<1|c>>>31}c=(h<<5|h>>>27)+j+l[a];c=20>a?c+((g&e|~g&k)+1518500249):40>a?c+((g^e^k)+1859775393):60>a?c+((g&e|g&k|e&k)-1894007588):c+((g^e^
+k)-899497514);j=k;k=e;e=g<<30|g>>>2;g=h;h=c}b[0]=b[0]+h|0;b[1]=b[1]+g|0;b[2]=b[2]+e|0;b[3]=b[3]+k|0;b[4]=b[4]+j|0},_doFinalize:function(){var f=this._data,e=f.words,b=8*this._nDataBytes,h=8*f.sigBytes;e[h>>>5]|=128<<24-h%32;e[(h+64>>>9<<4)+14]=Math.floor(b/4294967296);e[(h+64>>>9<<4)+15]=b;f.sigBytes=4*e.length;this._process();return this._hash},clone:function(){var e=j.clone.call(this);e._hash=this._hash.clone();return e}});e.SHA1=j._createHelper(m);e.HmacSHA1=j._createHmacHelper(m)})();
+
+(function(){var h=CryptoJS,j=h.lib.WordArray;h.enc.Base64={stringify:function(b){var e=b.words,f=b.sigBytes,c=this._map;b.clamp();b=[];for(var a=0;a<f;a+=3)for(var d=(e[a>>>2]>>>24-8*(a%4)&255)<<16|(e[a+1>>>2]>>>24-8*((a+1)%4)&255)<<8|e[a+2>>>2]>>>24-8*((a+2)%4)&255,g=0;4>g&&a+0.75*g<f;g++)b.push(c.charAt(d>>>6*(3-g)&63));if(e=c.charAt(64))for(;b.length%4;)b.push(e);return b.join("")},parse:function(b){var e=b.length,f=this._map,c=f.charAt(64);c&&(c=b.indexOf(c),-1!=c&&(e=c));for(var c=[],a=0,d=0;d<
+e;d++)if(d%4){var g=f.indexOf(b.charAt(d-1))<<2*(d%4),h=f.indexOf(b.charAt(d))>>>6-2*(d%4);c[a>>>2]|=(g|h)<<24-8*(a%4);a++}return j.create(c,a)},_map:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="}})();
+
 //
 
 /* WARNING: Changing any of the below variables will cause drastic
@@ -955,7 +1019,7 @@ w.s_ft=new Function("c","c+='';var s,e,o,a,d,q,f,h,x;s=c.indexOf('=function(');w
 +"'+c.substring(e+1);s=c.indexOf('=function(')}return c;");
 c=s_d(c);if(e>0){a=parseInt(i=v.substring(e+5));if(a>3)a=parseFloat(i)}else if(m>0)a=parseFloat(u.substring(m+10));else a=parseFloat(v);if(a<5||v.indexOf('Opera')>=0||u.indexOf('Opera')>=0)c=s_ft(c);if(!s){s=new Object;if(!w.s_c_in){w.s_c_il=new Array;w.s_c_in=0}s._il=w.s_c_il;s._in=w.s_c_in;s._il[s._in]=s;w.s_c_in++;}s._c='s_c';(new Function("s","un","pg","ss",c))(s,un,pg,ss);return s}
 function s_giqf(){var w=window,q=w.s_giq,i,t,s;if(q)for(i=0;i<q.length;i++){t=q[i];s=s_gi(t.oun);s.sa(t.un);s.setTagContainer(t.tagContainerName)}w.s_giq=0}s_giqf()
-// End s_code
+ // End s_code
 
 //tealium universal tag - utag.sender.19004.h27 v4.0.##UTVERSION##, Copyright ##UTYEAR## Tealium.com Inc. All Rights Reserved.
 try{
